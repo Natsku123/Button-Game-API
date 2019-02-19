c6aab0804f1d5e1bb83d3a8ea825c635e1987fe4
Max Mecklin <max@meckl.in>
2019-02-19 18:16:00 +0200

    Fix installer

--- install.py
+++ install.py
@@ -133,7 +133,7 @@ def main():
                              cursorclass=pymysql.cursors.DictCursor)
         try:
             with db.cursor() as cursor:
-                cursor.execute(command)
+                cursor.execute(command.strip("\n"))
 
             db.commit()
             db.close()
